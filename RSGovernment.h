40731251ab545d2163b5f1b59a6b5dab3299975d
Romito2015 <romito@live.ru>
2015-07-07 16:18:36 +0300

    Very first commit - init

---
+++ RSGovernment.h
@@ -0,0 +1,15 @@
+//
+//  RSGovernment.h
+//  10LessonNotificationsHW
+//
+//  Created by Roman Chopovenko on 07.07.15.
+//  Copyright (c) 2015 Roman Chopovenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RSGovernment : NSObject
+
+@property (assign, nonatomic) CGFloat taxLevel;
+
+@end
---
+++ RSGovernment.m
@@ -0,0 +1,13 @@
+//
+//  RSGovernment.m
+//  10LessonNotificationsHW
+//
+//  Created by Roman Chopovenko on 07.07.15.
+//  Copyright (c) 2015 Roman Chopovenko. All rights reserved.
+//
+
+#import "RSGovernment.h"
+
+@implementation RSGovernment
+
+@end
