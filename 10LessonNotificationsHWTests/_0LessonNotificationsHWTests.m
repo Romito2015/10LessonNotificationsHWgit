92e26cb90ba6a3d9a88be33c2f3df32116d608ff
Romito2015 <romito@live.ru>
2015-07-07 16:15:59 +0300

    Initial Commit

---
+++ 10LessonNotificationsHWTests/_0LessonNotificationsHWTests.m
@@ -0,0 +1,34 @@
+//
+//  _0LessonNotificationsHWTests.m
+//  10LessonNotificationsHWTests
+//
+//  Created by Roman Chopovenko on 07.07.15.
+//  Copyright (c) 2015 Roman Chopovenko. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _0LessonNotificationsHWTests : XCTestCase
+
+@end
+
+@implementation _0LessonNotificationsHWTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
